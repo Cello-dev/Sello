fed9fc5d2bc36b90268e57f92d53e9c3b04d01b7
C400 <tblaze28@gmail.com>
2022-06-27 00:57:52 -0400

    Adjusting and adding new fields

--- objects/Business.js
+++ objects/Business.js
@@ -1,33 +1,59 @@
 module.exports = class Business {
-    constructor(name,handle, address, email, password, productList, selloReq) {
+
+    constructor(name, handle, address, email, img_url, created_date, last_login_date, view_count, productList, selloReq) {
         this.name = name;
         this.handle = handle;
         this.address = address;
         this.email = email;
-        this.password = password;
+        this.img_url = img_url;
+        this.created_date = created_date;
+        this.last_login_date = last_login_date;
+        this.view_count = view_count;
         this.productList = productList;
-        this.isLoggedIn = True;
-        this.selloReq; 
+        this.isLoggedIn = true;
+        this.selloReq = selloReq;
     }
 
     get getName() {
         return this.name;
     }
+    
     get getHandle(){
-        return "$"+this.Handle
+        return "$"+this.handle
     }
+    
     get getAddress(){
         return this.address;
     }
+    
     get getEmail(){
         return this.email;
     }
+
+    get getImgUrl() {
+        return this.img_url;
+    }
+
+    get getCreatedDate() {
+        return this.created_date;
+    }
+
+    get getLastLoginDate() {
+        return this.last_login_date;
+    }
+
+    get getViewCount() {
+        return this.view_count;
+    }
+    
     get getProductList() {
         return this.productList;
     }
+    
     get isLoggedin() {
         return this.isLoggedIn;
     }
+
     getProduct(product){
         productinst =  this.productList.prototype.find(product);
         return productinst;
--- objects/Product.js
+++ objects/Product.js
@@ -1,28 +1,45 @@
 module.exports = class Product {
-    constructor(name, business, taglist, description, imgURL){
+
+    constructor(name, business, taglist, description, img_url, created_date, last_modified){
         this.name = name;
-        this.business= business; 
+        this.business= business;
         this.taglist = taglist;
         this.description = description;
-        this.imgURL = imgURL; 
+        this.img_url = img_url;
+        this.created_date = created_date;
+        this.last_modified = last_modified;
     }
+    
     get getName() {
         return this.name;
     }
+    
     get getBusiness(){
         return this.business;
     }
+    
     get getTaglist(){
         return this.taglist;
     }
+    
     getTag(i){
         tag = taglist[i];
         return tag;
     }
+    
     get getDescription(){
         return this.description; 
     }
-    get getimgURL(){
-        return this.imgURL;
+    
+    get getImgUrl(){
+        return this.img_url;
+    }
+
+    get getCreatedDate() {
+        return this.created_date;
+    }
+
+    get getLastModified() {
+        return this.last_modified;
     }
 }
