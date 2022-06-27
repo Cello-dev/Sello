885702a57c58fdf04be8eac6025d28b761ac5843
C400 <tblaze28@gmail.com>
2022-06-27 00:59:09 -0400

    Cookie search and CSRFToken added

---
+++ components/requests.js
@@ -0,0 +1,11 @@
+
+const options =  {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+};
+
